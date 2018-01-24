8a4f02bbbdff6ef7b848acb19cb96404f46a76c4
dev_fsc <fco.alvarez06@gmail.com>
2018-01-24 13:13:04 -0600

    Revert "Fix bugs in interface"
    This reverts commit e69b780e1d2dfe615424254f95e678abb2be6519.

--- src/qt/paccoinstrings.cpp
+++ src/qt/paccoinstrings.cpp
@@ -42,7 +42,7 @@ QT_TRANSLATE_NOOP("paccoin-core", ""
 "notation for IPv6. This option can be specified multiple times (default: "
 "bind to all interfaces)"),
 QT_TRANSLATE_NOOP("paccoin-core", ""
-"Cannot obtain a lock on data directory %s. $PAC Core is probably already "
+"Cannot obtain a lock on data directory %s. Paccoin Core is probably already "
 "running."),
 QT_TRANSLATE_NOOP("paccoin-core", ""
 "Create new files with system default permissions, instead of umask 077 (only "
@@ -51,7 +51,7 @@ QT_TRANSLATE_NOOP("paccoin-core", ""
 "Delete all wallet transactions and only recover those parts of the "
 "blockchain through -rescan on startup"),
 QT_TRANSLATE_NOOP("paccoin-core", ""
-"Disable all $PAC specific functionality (Masternodes, PrivateSend, "
+"Disable all Paccoin specific functionality (Masternodes, PrivateSend, "
 "InstantSend, Governance) (0-1, default: %u)"),
 QT_TRANSLATE_NOOP("paccoin-core", ""
 "Discover own IP addresses (default: 1 when listening and no -externalip or -"
@@ -158,7 +158,7 @@ QT_TRANSLATE_NOOP("paccoin-core", ""
 "Output debugging information (default: %u, supplying <category> is optional)"),
 QT_TRANSLATE_NOOP("paccoin-core", ""
 "Please check that your computer's date and time are correct! If your clock "
-"is wrong $PAC Core will not work properly."),
+"is wrong Paccoin Core will not work properly."),
 QT_TRANSLATE_NOOP("paccoin-core", ""
 "PrivateSend uses exact denominated amounts to send funds, you might simply "
 "need to anonymize some more coins."),
@@ -221,7 +221,7 @@ QT_TRANSLATE_NOOP("paccoin-core", ""
 "Tries to keep outbound traffic under the given target (in MiB per 24h), 0 = "
 "no limit (default: %d)"),
 QT_TRANSLATE_NOOP("paccoin-core", ""
-"Unable to bind to %s on this computer. $PAC Core is probably already running."),
+"Unable to bind to %s on this computer. Paccoin Core is probably already running."),
 QT_TRANSLATE_NOOP("paccoin-core", ""
 "Unable to locate enough PrivateSend denominated funds for this transaction."),
 QT_TRANSLATE_NOOP("paccoin-core", ""
@@ -358,7 +358,7 @@ QT_TRANSLATE_NOOP("paccoin-core", "Error loading %s: You can't disable HD on a a
 QT_TRANSLATE_NOOP("paccoin-core", "Error loading block database"),
 QT_TRANSLATE_NOOP("paccoin-core", "Error loading wallet.dat"),
 QT_TRANSLATE_NOOP("paccoin-core", "Error loading wallet.dat: Wallet corrupted"),
-QT_TRANSLATE_NOOP("paccoin-core", "Error loading wallet.dat: Wallet requires newer version of $PAC Core"),
+QT_TRANSLATE_NOOP("paccoin-core", "Error loading wallet.dat: Wallet requires newer version of Paccoin Core"),
 QT_TRANSLATE_NOOP("paccoin-core", "Error opening block database"),
 QT_TRANSLATE_NOOP("paccoin-core", "Error reading from database, shutting down."),
 QT_TRANSLATE_NOOP("paccoin-core", "Error"),
@@ -385,7 +385,7 @@ QT_TRANSLATE_NOOP("paccoin-core", "Incompatible mode."),
 QT_TRANSLATE_NOOP("paccoin-core", "Incompatible version."),
 QT_TRANSLATE_NOOP("paccoin-core", "Incorrect or no genesis block found. Wrong datadir for network?"),
 QT_TRANSLATE_NOOP("paccoin-core", "Information"),
-QT_TRANSLATE_NOOP("paccoin-core", "Initialization sanity check failed. $PAC Core is shutting down."),
+QT_TRANSLATE_NOOP("paccoin-core", "Initialization sanity check failed. Paccoin Core is shutting down."),
 QT_TRANSLATE_NOOP("paccoin-core", "Input is not valid."),
 QT_TRANSLATE_NOOP("paccoin-core", "InstantSend options:"),
 QT_TRANSLATE_NOOP("paccoin-core", "Insufficient funds."),
@@ -537,7 +537,7 @@ QT_TRANSLATE_NOOP("paccoin-core", "Verifying wallet..."),
 QT_TRANSLATE_NOOP("paccoin-core", "Very low number of keys left: %d"),
 QT_TRANSLATE_NOOP("paccoin-core", "Wallet %s resides outside data directory %s"),
 QT_TRANSLATE_NOOP("paccoin-core", "Wallet is locked."),
-QT_TRANSLATE_NOOP("paccoin-core", "Wallet needed to be rewritten: restart $PAC Core to complete"),
+QT_TRANSLATE_NOOP("paccoin-core", "Wallet needed to be rewritten: restart Paccoin Core to complete"),
 QT_TRANSLATE_NOOP("paccoin-core", "Wallet options:"),
 QT_TRANSLATE_NOOP("paccoin-core", "Wallet window title"),
 QT_TRANSLATE_NOOP("paccoin-core", "Warning"),
